9436aa12840de12c965ddae6a5a4d2f581fe14c7
Iván Guillén <ivan.guillen.moreno@gmail.com>
2025-09-25 11:03:26 +0200

    Installed tailwindcss

--- src/App.js
+++ src/App.js
@@ -12,27 +12,30 @@ const repo = new DgtRepository();
 
 function App() {
   return (
-    <Router basename="/dgt-test-trainer">
-        <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
-          <Link to="/">Inicio</Link>
-          <Link to="/test">Simulador Test</Link>
-          <Link to="/anki">Método Anki</Link>
-          <Link to="/user-statistics">Estadísticas del usuario</Link>
-          <Link to="/user-settings" style={{marginLeft: "auto"}}>X</Link>
-
-        </nav>
-
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/test" element={<TestSimulator />} />
-        <Route path="/anki" element={<AnkiMethod />} />
-        <Route path="/user-statistics" element={<UserStatistics />} />
-        <Route path="/user-settings" element={<UserSettings />} />
+     <div className="min-h-screen bg-gray-50 text-gray-900">
+      <div className="max-w-5xl mx-auto px-4 py-6">
+        <Router basename="/dgt-test-trainer">
+            <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
+              <Link to="/">Inicio</Link>
+              <Link to="/test">Simulador Test</Link>
+              <Link to="/anki">Método Anki</Link>
+              <Link to="/user-statistics">Estadísticas del usuario</Link>
+              <Link to="/user-settings" style={{marginLeft: "auto"}}>X</Link>
+            </nav>
 
 
-      </Routes>
-    </Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<TestSimulator />} />
+            <Route path="/anki" element={<AnkiMethod />} />
+            <Route path="/user-statistics" element={<UserStatistics />} />
+            <Route path="/user-settings" element={<UserSettings />} />
+
+
+          </Routes>
+        </Router>
+        </div>
+    </div>
   );
 }
 
